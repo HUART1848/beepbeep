ce3e4dd789d2927c7d2c5fe78fb9c988b89903a1
Hugo Huart <hugo.huart@heig-vd.ch>
2023-02-05 15:56:28 +0100

    Add notes

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,48 @@
-use std::io::{stdin, stdout, Result, Stdout, Write};
+use std::io::{stdin, stdout, Stdout, Write};
 use std::process::{Command, Output};
+use std::result::Result;
+
 use termion::event::Key;
 use termion::input::TermRead;
 use termion::raw::{IntoRawMode, RawTerminal};
 
-fn beep() -> Result<Output> {
-    return Command::new("beep").arg("-f").arg("440").output();
+enum Notes {
+    C, D, E, F, G, A, B
+}
+
+impl Notes {
+    fn from_key(key: char) -> Result<Self, ()> {
+        return match key {
+            'a' => Ok(Notes::C),
+            's' => Ok(Notes::D),
+            'd' => Ok(Notes::E),
+            'f' => Ok(Notes::F),
+            'g' => Ok(Notes::G),
+            'h' => Ok(Notes::A),
+            'j' => Ok(Notes::B),
+            _ => Err(())
+        }
+    }
+
+    fn to_freq(&self) -> &'static str {
+        return match self {
+            Notes::C => "261",
+            Notes::D => "293",
+            Notes::E => "329",
+            Notes::F => "349",
+            Notes::G => "391",
+            Notes::A => "440",
+            Notes::B => "493"
+        }
+    }
+}
+
+fn beep(letter: char) -> Result<Output, ()> {
+    let freq = Notes::from_key(letter)?.to_freq();
+    return match Command::new("beep").arg("-f").arg(freq).output() {
+        Ok(out) => Ok(out),
+        Err(_) => Err(()) 
+    };
 }
 
 fn clear_screen(toclear: &mut RawTerminal<Stdout>) {
@@ -26,15 +63,15 @@ fn main() {
         clear_screen(&mut stdout);
         match c.unwrap() {
             Key::Ctrl('q') => break,
-            Key::Ctrl('h') => write!(stdout, "Help!\r\n").unwrap(),
             Key::Alt('t') => write!(stdout, "T-Rex\r\n").unwrap(),
-            _ => match beep() {
+            Key::Char(x) => match beep(x) {
                 Err(_) => {
                     write!(stdout, "beep failed... exiting\r\n").unwrap();
                     break;
                 }
                 _ => (),
             },
+            _ => ()
         }
         stdout.flush().unwrap();
     }
